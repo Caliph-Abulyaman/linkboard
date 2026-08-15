b24fb10bb436a023f4668de020624434a38924d3
Abul Yaman <abulyaman101@gmail.com>
2026-08-16 01:08:05 +0500

    feat: wire auth routes and cookie parsing into app

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const pool = require('./db');
+const authRoutes = require('./auth');
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use('/auth', authRoutes);
 
 app.get('/health', async (req, res) => {
     try {
